450ba007315616ec38808f98ead971e586827614
IsmaelLG <ismael.llopart@gmail.com>
2022-05-20 14:08:45 +0200

    updated

--- src/main/java/edu/n3/exercise_1/Generic.java
+++ src/main/java/edu/n3/exercise_1/Generic.java
@@ -2,15 +2,13 @@ package edu.n3.exercise_1;
 
 public class Generic {
 
-	public void call(Phone phone) {
+	public <T extends Phone> void call(T phone) {
 		phone.call("987654321");
-	//	phone.toPhotograph();   
-	//	SmartPhone smartphone=(SmartPhone)phone;
-	//	smartphone.toPhotograph();   
-		System.out.println("El mètode call no pot invocar al mètode to Photograph tret que es castegi.");
+	//	phone.toPhotograph();
+		System.out.println("El objecte no pot invocar al mètode toPhotograph. Error de compilació: \"The method toPhotograph() is undefined for the type T\"");
 	}
 	
-	public void toPhotograph(SmartPhone smartphone) {
+	public <T extends SmartPhone> void toPhotograph(T smartphone) {
 		smartphone.toPhotograph();
 	}
 }
