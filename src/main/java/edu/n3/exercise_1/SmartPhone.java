3b29d8f9405acdad68a59a250a6f68f8e3f885ad
IsmaelLG <ismael.llopart@gmail.com>
2022-05-23 11:21:39 +0200

    camera

--- src/main/java/edu/n3/exercise_1/SmartPhone.java
+++ src/main/java/edu/n3/exercise_1/SmartPhone.java
@@ -1,13 +1,13 @@
 package edu.n3.exercise_1;
 
-public class SmartPhone implements Phone {
+public class SmartPhone implements Phone,Camera {
 
 	@Override
 	public void call(String phoneNumber) {
 		System.out.println("Trucant al número " + phoneNumber);
 	}
 
-
+	@Override
 	public void toPhotograph() {
 		System.out.println("S'està fent una foto.");
 	}
